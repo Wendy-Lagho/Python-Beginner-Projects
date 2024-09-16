f2e6dc49412e6a4a0fee86693e670425f6da270f
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-09-16 22:36:58 +0300

    File Handling

---
+++ File Handling/functions.py
@@ -0,0 +1,89 @@
+import re
+import pandas as pd
+
+def combine_data_frames(df1, df2):
+    """
+    Combine two DataFrames and return the combined result.
+    :param df1: First DataFrame
+    :param df2: Second DataFrame
+    :return: Combined DataFrame
+    """
+    combined_df = pd.concat([df1, df2], ignore_index=True)
+    return combined_df
+
+def generate_email(name):
+    """
+    Generate an email from the student name.
+    Takes the first letter of the first name and combines it with the last name
+    :param name: student name
+    :return: email
+    """
+    name_parts = re.split(r'[\s,]', name)
+    email_name = name_parts[0][0].lower() + name_parts[-1].lower()
+    return f'{email_name}@gmail.com'
+
+def make_unique(emails):
+    """
+    Make email addresses unique by appending a number if duplicates exist.
+    :param emails: list of email addresses
+    :return: unique list of email addresses
+    """
+    seen = {}
+    unique_emails = []
+
+    for email in emails:
+        if email not in seen:
+            seen[email] = 0
+            unique_emails.append(email)
+        else:
+            seen[email] += 1
+            unique_email = f'{email.split('@')[0]}{seen[email]}@gmail.com'
+            unique_emails.append(unique_email)
+
+    return unique_emails
+
+def get_students_by_gender(df, gender):
+    """
+    Get all students by a specified gender.
+    :param df: Dataframe containing student names
+    :param gender: M or F
+    :return: DataFrame containing students of the specified gender
+    """
+    for  index, row in df.iterrows():
+        if row['Gender'] == gender:
+            yield row
+
+def build_jsonl(shuffled_df, special_char_names):
+    """
+    Build data of shuffled students from the shuffled dataframe for jsonl
+    conversion
+    :param shuffled_df: The shuffled dataframe
+    :param special_char_names: The list of names with special characters
+    :return:A list of dictionaries containing shuffled student information
+    """
+    shuffled_students = []
+    id_count = 0
+
+    for index, row in shuffled_df.iterrows():
+        student_name = row["Student Name"]
+        if student_name in special_char_names:
+            has_special_character = "['yes']"
+        else:
+            has_special_character = "['no']"
+
+        student_details = {
+            "id": id_count,
+            "student_number": row["Student Number"],
+            "additional_details": [
+                {
+                    "dob": row["DoB"].strftime("%Y-%m-%d"),
+                    "gender": row["Gender"],
+                    "special_character": has_special_character,
+                }
+            ]
+        }
+
+        shuffled_students.append(student_details)
+        id_count += 1
+
+    return shuffled_students
---
+++ File Handling/main.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import logging
+
+from numpy.ma.extras import unique
+
+from functions import generate_email, make_unique, combine_data_frames, get_students_by_gender, build_jsonl
+
+"""
+Logging configuration
+"""
+logging.basicConfig(
+    filename='computations.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
+"""
+Excel sheets
+"""
+df1 = pd.read_excel("File_A.xlsx")
+df2 = pd.read_excel("File_B.xlsx")
+df = combine_data_frames(df1, df2)
+
+"""
+Generating unique student emails
+"""
+df['Email Address'] = df['Student Name'].apply(generate_email)
+df['Email Address'] = make_unique(df['Email Address'])
+
+"""
+Logging generated emails
+"""
+logging.info("----- Generated Email Addresses -----")
+for index, row in df.iterrows():
+    logging.info(f"Student Email {index + 1}: {row['Email Address']}")
+
+"""
+Combined DataFrame saved as CSV and TSV
+"""
+df.to_csv("output/students.csv", index=False)
+df.to_csv("output/students.tsv", sep='\t', index=False)
+
+"""
+Male students
+"""
+male_df = pd.DataFrame(get_students_by_gender(df, "M"))
+male_df.to_csv("output/male_students.csv", index=False)
+logging.info(f"No of male students: {len(male_df)}")
+
+"""
+Female students
+"""
+female_df = pd.DataFrame(get_students_by_gender(df, "F"))
+female_df.to_csv("output/female_students.csv", index=False)
+logging.info(f"No of female students: {len(female_df)}")
+
+"""
+Students with special characters
+"""
+pattern = r"[^\w\s,]"
+special_char_students = df[df["Student Name"].str.contains(pattern, regex=True)]
+
+special_char_names = []
+logging.info(f"Students with special characters in their names: {len(special_char_students)}")
+for student_name in special_char_students["Student Name"]:
+    logging.info(f" - {student_name}")
+    special_char_names.append(student_name)
+
+"""
+Save to CSV
+"""
+special_char_students.to_csv("output/special_char_students.csv", index=False)
+
+"""
+One-time shuffle
+"""
+shuffled_df = df.sample(frac=1)
+
+"""
+Output shuffled data to JSON
+"""
+shuffled_df.to_json("output/shuffled_students.json", orient="records")
+
+"""
+Building data to convert to JSONL
+"""
+shuffled_students = build_jsonl(shuffled_df, special_char_names)
+
+shuffled_df = pd.DataFrame(shuffled_students)
+shuffled_df.to_json("output/shuffled_students.jsonl", orient="records", lines=True)
